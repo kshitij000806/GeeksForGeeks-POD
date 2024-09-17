787b958da963036ed76526504405d04d9db6281d
Kshitij M.K <85734422+kshitij000806@users.noreply.github.com>
2024-09-17 22:59:08 +0530

    Create 17SEP.py

---
+++ 17SEP.py
@@ -0,0 +1,17 @@
+class Solution:
+    def getMinDiff(self, arr, k):
+        n = len(arr)
+        if n == 1:
+            return 0
+        arr.sort()
+        ans = arr[-1] - arr[0]
+        smallest = arr[0] + k
+        largest = arr[-1] - k
+        for i in range(n - 1):
+            minVal = min(smallest, arr[i + 1] - k)
+            maxVal = max(largest, arr[i] + k)
+            if minVal < 0:
+                continue
+            ans = min(ans, maxVal - minVal)
+
+        return ans
